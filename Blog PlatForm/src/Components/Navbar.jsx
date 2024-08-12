855761159efdbbb087150de6f7a71e6cbab441dc
Ajit raut <ajitraut9561@gmail.com>
2024-08-12 22:31:06 +0530

    Router Added

--- Blog PlatForm/src/Components/Navbar.jsx
+++ Blog PlatForm/src/Components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
-    <div className="container mx-auto flex justify-between items-center">
+    <div className="max-w-7xl mx-auto flex justify-between items-center">
       <div className="text-lg font-bold">MyBlog</div>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">Home</Link>
+        {/* <Link to="/" className="hover:underline">Home</Link> */}
         <Link to="/categories" className="hover:underline">Categories</Link>
-        <Link to="/tags" className="hover:underline">Tags</Link>
-        <Link to="/profile" className="hover:underline">Profile</Link>
+        {/* <Link to="/tags" className="hover:underline">Tags</Link>
+        <Link to="/profile" className="hover:underline">Profile</Link> */}
       </div>
     </div>
   </nav>
--- Blog PlatForm/src/main.jsx
+++ Blog PlatForm/src/main.jsx
@@ -1,8 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import * as ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+import Categories from "./Components/Categories.jsx";
+const AppRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+       
+        {
+          path: "/categories",
+          element: <Categories />,
+        },
+      ],
+    },
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={AppRouter} />
+  );
+  
