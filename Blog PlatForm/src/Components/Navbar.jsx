16428e9dddae95852d41c1451b97dd5c7be6d3ad
Ajit raut <ajitraut9561@gmail.com>
2024-08-12 22:33:38 +0530

    Profile & Tags Component Adde

--- Blog PlatForm/src/Components/Navbar.jsx
+++ Blog PlatForm/src/Components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
     <div className="max-w-7xl mx-auto flex justify-between items-center">
       <div className="text-lg font-bold">MyBlog</div>
       <div className="space-x-4">
-        {/* <Link to="/" className="hover:underline">Home</Link> */}
+        <Link to="/" className="hover:underline">Home</Link>
         <Link to="/categories" className="hover:underline">Categories</Link>
-        {/* <Link to="/tags" className="hover:underline">Tags</Link>
-        <Link to="/profile" className="hover:underline">Profile</Link> */}
+        <Link to="/tags" className="hover:underline">Tags</Link>
+        <Link to="/profile" className="hover:underline">Profile</Link>
       </div>
     </div>
   </nav>
