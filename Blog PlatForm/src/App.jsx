8dfc45b589becbff26638c0d556adb9a38113925
Ajit raut <ajitraut9561@gmail.com>
2024-08-14 22:52:41 +0530

    Profile & Tags Component Added to router

--- Blog PlatForm/src/App.jsx
+++ Blog PlatForm/src/App.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Homepage from './Components/Homepage'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Outlet />
+      <Homepage/>
     </>
   )
 }
