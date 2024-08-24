67847f5aeab327d354541111c6f97dbbe70ea75b
Ajit raut <ajitraut9561@gmail.com>
2024-08-24 18:20:12 +0530

    Post Header Added

---
+++ Blog PlatForm/src/Components/PostHeader.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const PostHeader = ({ post }) => {
+  const formatDate = (date) => {
+    if (date instanceof Date) {
+      const options = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      };
+      return new Intl.DateTimeFormat("en-GB", options).format(date);
+    } else if (date?.seconds) {
+      const options = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      };
+      return new Intl.DateTimeFormat("en-GB", options).format(
+        new Date(date.seconds * 1000)
+      );
+    } else {
+      return "Date not available";
+    }
+  };
+
+  const formattedDate = post.createdAt
+    ? formatDate(new Date(post.createdAt.toDate()))
+    : "Date not available";
+
+  return (
+    <div>
+      <h3 className="text-xl font-bold mb-2 text-gray-900 dark:text-white">
+        {post.title}
+      </h3>
+      {post.imageUrl && (
+        <img
+          src={post.imageUrl}
+          alt={post.title}
+          className="w-full h-auto mb-2 rounded"
+        />
+      )}
+      <p className="mt-2 text-gray-600 dark:text-gray-400">
+        By {post.author} | {formattedDate}
+      </p>
+    </div>
+  );
+};
+
+export default PostHeader;
